98192705865cc64802d7fc3409ba15ba4d069c47
tjm <tjm@cantab.net>
2020-11-05 13:33:10 +0000

    Create planetary frames for the player as needed.

---
+++ Assets/MoonShot/Scripts/Planet/OrreryPlanet.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Moonshot.World;
+using System;
+using UnityEngine.Animations;
+
+namespace Moonshot.Planet
+{
+	public class OrreryPlanet : MonoBehaviour
+	{
+		public Color PlanetColor = Color.white;
+		public float PlanetIntensity = 1;
+		public float Radius = 100.0f;
+
+		public GameObject HighResPlanetPrefab;
+		public GameObject LightPrefab;
+
+		public List<OrreryPlanet> LightSources;
+
+		public bool CanConstructFrame => HighResPlanetPrefab != null;
+
+		public LocalFrame ConstructLocalFrame()
+		{
+			if (!CanConstructFrame)
+			{
+				return null;
+			}
+
+			GameObject frameObject = CreateObject(gameObject.name + " Local Frame", null);
+			var frame = frameObject.AddComponent<LocalFrame>();
+			frame.GlobalLocation = transform;
+
+			CreateHighResPlanet(frame);
+			SetupLighting(frame);
+			PopulateObjects(frame);
+
+			return frame;
+		}
+
+		private void CreateHighResPlanet(LocalFrame frame)
+		{
+			Instantiate(HighResPlanetPrefab, frame.transform, false);
+
+			// TODO - Initialise high res planet
+		}
+
+		private void SetupLighting(LocalFrame frame)
+		{
+			foreach (var planet in LightSources)
+			{
+				planet.SetupLightForOtherLocalFrame(frame);
+			}
+		}
+
+		private void PopulateObjects(LocalFrame frame)
+		{
+			// Nothing right now
+		}
+
+		private void SetupLightForOtherLocalFrame(LocalFrame frame)
+		{
+			GameObject lightObject = Instantiate(LightPrefab, frame.transform, false);
+			lightObject.name = gameObject.name + " Light";
+
+			Light light = lightObject.GetComponent<Light>();
+			light.color = PlanetColor;
+			light.intensity = PlanetIntensity; // TODO: Intensity falloff by distance
+
+			LookAtConstraint lac = lightObject.GetComponent<LookAtConstraint>();
+			if (lac == null)
+			{
+				lac = lightObject.AddComponent<LookAtConstraint>();
+			}
+			ConstraintSource cs = new ConstraintSource();
+			cs.sourceTransform = frame.transform;
+			cs.weight = 1.0f;
+			lac.AddSource(cs);
+			lac.constraintActive = true;
+
+			CrossFramePositionConstraint cfpc = lightObject.GetComponent<CrossFramePositionConstraint>();
+			if (cfpc == null)
+			{
+				cfpc = lightObject.AddComponent<CrossFramePositionConstraint>();
+			}
+			cfpc.m_target = transform;
+		}
+
+		private GameObject CreateObject(string i_name, Transform i_parent)
+		{
+			var obj = new GameObject(i_name);
+			if (i_parent)
+			{
+				obj.transform.SetParent(i_parent);
+			}
+			obj.transform.localPosition = Vector3.zero;
+			obj.transform.localRotation = Quaternion.identity;
+			obj.transform.localScale = Vector3.one;
+			return obj;
+		}
+
+	}
+}
---
+++ Assets/MoonShot/Scripts/Player/PlayerFrameManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Moonshot.Planet;
+using Moonshot.World;
+using System.Linq;
+
+namespace Moonshot.Player
+{
+	public class PlayerFrameManager : MonoBehaviour
+	{
+		public float m_planetFrameRange = 300.0f;
+
+		void Start()
+		{
+			UpdateFrame(true);
+		}
+
+		void Update()
+		{
+			UpdateFrame(false);
+		}
+
+		void UpdateFrame(bool i_instant)
+		{
+			LocalFrame currentFrame = LocalFrame.Get(transform);
+
+			if (ShouldSwitchFrames(currentFrame))
+			{
+				LocalFrame newFrame = CreateSuitableLocalFrame(currentFrame);
+				ExitFrame(currentFrame);
+				EnterFrame(newFrame);
+				if (currentFrame)
+				{
+					Destroy(currentFrame);
+				}
+			}
+		}
+
+		private bool ShouldSwitchFrames(LocalFrame currentFrame)
+		{
+			// TODO - Other reasons
+			return currentFrame == null;
+		}
+
+		private LocalFrame CreateSuitableLocalFrame(LocalFrame currentFrame)
+		{
+			IEnumerable<OrreryPlanet> planets = FindObjectsOfType<OrreryPlanet>();
+
+			Vector3 globalPos = LocalFrame.TransformPointToGlobal(currentFrame, transform.position);
+			var nearestPlanet = planets.Where(p => p.CanConstructFrame).OrderBy(p => (p.transform.position - globalPos).magnitude - p.Radius).FirstOrDefault();
+
+			if (nearestPlanet)
+			{
+				float distanceToSurface = (nearestPlanet.transform.position - globalPos).magnitude - nearestPlanet.Radius;
+
+				if (distanceToSurface < m_planetFrameRange)
+				{
+					var newFrame = nearestPlanet.ConstructLocalFrame();
+					if (newFrame)
+					{
+						return newFrame;
+					}
+				}
+			}
+
+			return ConstructSpaceFrame();
+		}
+
+		private LocalFrame ConstructSpaceFrame()
+		{
+			// TODO
+			return null;
+		}
+
+		private void ExitFrame(LocalFrame oldFrame)
+		{
+			if (oldFrame)
+			{
+				Vector3 globalPos = oldFrame.TransformPointToGlobal(transform.position);
+				Quaternion globalRot = oldFrame.TransformRotationToGlobal(transform.rotation);
+				transform.SetParent(null);
+				transform.SetPositionAndRotation(globalPos, globalRot);
+			}
+		}
+
+		private void EnterFrame(LocalFrame newFrame)
+		{
+			if (newFrame)
+			{
+				Vector3 localPos = newFrame.TransformPointToLocal(transform.position);
+				Quaternion localRot = newFrame.TransformRotationToLocal(transform.rotation);
+				transform.SetParent(newFrame.transform);
+				transform.localPosition = localPos;
+				transform.localRotation = localRot;
+			}
+		}
+	}
+}
