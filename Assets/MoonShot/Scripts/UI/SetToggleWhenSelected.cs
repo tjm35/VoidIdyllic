e6ed47be6be7e2af93afc0542a973a42f7a13081
tjm <tjm@cantab.net>
2020-11-06 08:17:10 +0000

    Streamline UI tab selection and logic.

---
+++ Assets/MoonShot/Scripts/UI/SetToggleWhenSelected.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Moonshot.UI
+{
+	[RequireComponent(typeof(Toggle))]
+	public class SetToggleWhenSelected : MonoBehaviour, ISelectHandler
+	{
+		// Start is called before the first frame update
+		void Awake()
+		{
+			m_toggle = GetComponent<Toggle>();
+		}
+
+		public void OnSelect(BaseEventData eventData)
+		{
+			m_toggle.isOn = true;
+		}
+
+		private Toggle m_toggle;
+	}
+}
