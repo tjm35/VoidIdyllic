e43563757f5e54726f2cdc40c1e7375b2cd4de57
tjm <tjm@cantab.net>
2020-11-11 10:02:24 +0000

    Make launch menu option dismiss menu.

--- Assets/MoonShot/Scripts/UI/ShipTakeoffUITrigger.cs
+++ Assets/MoonShot/Scripts/UI/ShipTakeoffUITrigger.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Moonshot.Ship;
+using PlayMaker;
 using UnityEngine;
 
 namespace Moonshot.UI
 {
 	public class ShipTakeoffUITrigger : MonoBehaviour
 	{
+		public PlayMakerFSM m_menuFSM;
+
 		public void PerformTakeoff()
 		{
 			var takeoff = transform.GetComponentInAncestors<ShipTakeoff>();
 			if (takeoff)
 			{
 				takeoff.PerformTakeoff();
+			}
+			if (m_menuFSM)
+			{
+				m_menuFSM.SendEvent("ExitMenu");
 			}
 		}
 	}
