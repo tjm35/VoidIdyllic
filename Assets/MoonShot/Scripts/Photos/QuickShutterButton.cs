672f3300d669b40f1fb47abcfc3c7678cea7eb42
tjm <tjm@cantab.net>
2020-11-12 19:26:28 +0000

    Camera shutter animation. (Could be done in a better way, but this will do for now.)

--- Assets/MoonShot/Scripts/Photos/QuickShutterButton.cs
+++ Assets/MoonShot/Scripts/Photos/QuickShutterButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 namespace Moonshot.Photos
@@ -10,6 +11,8 @@ namespace Moonshot.Photos
 		public PhotoSystem m_photoSystem;
 		public Camera m_camera;
 		public InputActionReference m_shutterButton;
+		public UnityEvent m_onShutterClicked = new UnityEvent();
+		public Animation m_shutterAnimation;
 
 		// Update is called once per frame
 		void Start()
@@ -24,6 +27,11 @@ namespace Moonshot.Photos
 		void TakePhoto(InputAction.CallbackContext i_context)
 		{ 
 			m_photoSystem.TakePhoto(m_camera);
+			m_onShutterClicked.Invoke();
+			if (m_shutterAnimation)
+			{
+				m_shutterAnimation.Play();
+			}
 		}
 	}
 }
