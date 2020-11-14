825e68f9ea760ccecd76a96447680d956f9c8405
tjm <tjm@cantab.net>
2020-11-14 15:46:22 +0000

    Use prefabs and code propagation to somewhat standardise UI look.

---
+++ Assets/MoonShot/Scripts/UI/UIColorSetup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Moonshot.UI
+{
+	public class UIColorSetup : MonoBehaviour, UIColors.Settable
+	{
+		public UIColors.ColorType ColorType = UIColors.ColorType.Primary;
+		public bool SetupOnStart = true;
+
+		public void SetupColors(UIColors i_colors)
+		{
+			var col = i_colors.Get(ColorType);
+			ApplyColor(col);
+		}
+
+		private void Start()
+		{
+			if (SetupOnStart)
+			{
+				var uicolors = transform.GetComponentInAncestors<UIColors>();
+				if (uicolors)
+				{
+					SetupColors(uicolors);
+				}
+			}
+		}
+
+		private void ApplyColor(Color i_col)
+		{
+			var graphic = GetComponent<Graphic>();
+			if (graphic)
+			{
+				graphic.color = i_col;
+			}
+		}
+	}
+}
---
+++ Assets/MoonShot/Scripts/UI/UIColors.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.UI
+{
+	public class UIColors : MonoBehaviour
+	{
+		public interface Settable
+		{
+			void SetupColors(UIColors i_colors);
+		}
+
+		public enum ColorType
+		{
+			Primary,
+			Background,
+			ReversePrimary,
+			Borders,
+		}
+
+		public Color Primary = Color.white;
+		public Color Background = Color.black;
+		public Color ReversePrimary = Color.black;
+		public Color Borders = Color.white;
+
+		public Color Get(ColorType i_type)
+		{
+			switch (i_type)
+			{
+				case ColorType.Primary:
+					return Primary;
+				case ColorType.Background:
+					return Background;
+				case ColorType.ReversePrimary:
+					return ReversePrimary;
+				case ColorType.Borders:
+					return Borders;
+			}
+			return new Color(1.0f, 0.6f, 0.8f);
+		}
+
+		[ContextMenu("Update Colors")]
+		public void UpdateColors()
+		{
+			foreach (var settable in transform.GetComponentsInDescendents<Settable>())
+			{
+				settable.SetupColors(this);
+			}
+		}
+	}
+}
