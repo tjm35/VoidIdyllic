28a0a375dd2689d010cdc5aa1c4d5be83e45858b
tjm <tjm@cantab.net>
2020-11-14 22:14:45 +0000

    Quick system for on-screen debugging.

---
+++ Assets/MoonShot/Scripts/UI/QuickDebug.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Moonshot.UI
+{
+	[RequireComponent(typeof(TMP_Text))]
+	public class QuickDebug : MonoBehaviour
+	{
+		public static void Print(string i_text)
+		{
+			if (s_instance)
+			{
+				s_instance.m_stringBuilder.AppendLine(i_text);
+			}
+		}
+
+		private void Start()
+		{
+			m_text = GetComponent<TMP_Text>();
+			s_instance = this;
+		}
+
+		private void OnDestroy()
+		{
+			s_instance = null;
+		}
+
+		private void LateUpdate()
+		{
+			m_text.text = m_stringBuilder.ToString();
+			m_stringBuilder.Clear();
+		}
+
+		private static QuickDebug s_instance;
+		private TMP_Text m_text;
+		private StringBuilder m_stringBuilder = new StringBuilder();
+	}
+}
