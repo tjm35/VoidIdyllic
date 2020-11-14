14f47729da562ef4f56e9a029ccf9c6439b63841
tjm <tjm@cantab.net>
2020-11-14 22:31:39 +0000

    Only take photos while aiming, and limit the rate at which photos can be taken.

--- Assets/MoonShot/Scripts/Photos/QuickShutterButton.cs
+++ Assets/MoonShot/Scripts/Photos/QuickShutterButton.cs
@@ -13,8 +13,8 @@ namespace Moonshot.Photos
 		public InputActionReference m_shutterButton;
 		public UnityEvent m_onShutterClicked = new UnityEvent();
 		public Animation m_shutterAnimation;
+		public float m_minPhotoInterval = 0.5f;
 
-		// Update is called once per frame
 		void Start()
 		{
 			InputAction action = m_shutterButton?.action;
@@ -24,14 +24,39 @@ namespace Moonshot.Photos
 			}
 		}
 
-		void TakePhoto(InputAction.CallbackContext i_context)
-		{ 
-			m_photoSystem.TakePhoto(m_camera);
-			m_onShutterClicked.Invoke();
-			if (m_shutterAnimation)
+		void OnDestroy()
+		{
+			InputAction action = m_shutterButton?.action;
+			if (action != null)
 			{
-				m_shutterAnimation.Play();
+				action.performed -= TakePhoto;
 			}
 		}
+
+		void TakePhoto(InputAction.CallbackContext i_context)
+		{
+			m_photoRequested = true;
+		}
+
+		private void Update()
+		{
+			m_photoElapsedTime += Time.deltaTime;
+
+			if (m_photoRequested && m_photoElapsedTime > m_minPhotoInterval)
+			{
+				m_photoSystem.TakePhoto(m_camera);
+				m_onShutterClicked.Invoke();
+				if (m_shutterAnimation)
+				{
+					m_shutterAnimation.Play();
+				}
+
+				m_photoRequested = false;
+				m_photoElapsedTime = 0.0f;
+			}
+		}
+
+		private bool m_photoRequested = false;
+		private float m_photoElapsedTime = 999.0f;
 	}
 }
