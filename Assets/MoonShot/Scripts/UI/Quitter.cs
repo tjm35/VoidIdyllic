cc8ac032a38915a153284f2666f06d0e86882cf9
tjm <tjm@cantab.net>
2020-11-15 22:31:36 +0000

    Add a UI option to exit the game. Propagate some settings down to their correct places in prefabs.

---
+++ Assets/MoonShot/Scripts/UI/Quitter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.UI
+{
+	public class Quitter : MonoBehaviour
+	{
+		public void Quit()
+		{
+			Application.Quit();
+		}
+	}
+}
--- Assets/MoonShot/Scripts/UI/UIColors.cs
+++ Assets/MoonShot/Scripts/UI/UIColors.cs
@@ -17,12 +17,14 @@ namespace Moonshot.UI
 			Background,
 			ReversePrimary,
 			Borders,
+			Alarm,
 		}
 
 		public Color Primary = Color.white;
 		public Color Background = Color.black;
 		public Color ReversePrimary = Color.black;
 		public Color Borders = Color.white;
+		public Color Alarm = Color.red;
 
 		public Color Get(ColorType i_type)
 		{
@@ -36,6 +38,8 @@ namespace Moonshot.UI
 					return ReversePrimary;
 				case ColorType.Borders:
 					return Borders;
+				case ColorType.Alarm:
+					return Alarm;
 			}
 			return new Color(1.0f, 0.6f, 0.8f);
 		}
