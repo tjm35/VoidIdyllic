bed5cf6454f6eefb6b6e399d50b95cc8b8355175
tjm <tjm@cantab.net>
2020-11-19 23:07:54 +0000

    Add (disabled) bonus goals for ship selfie and all bodies.

--- Assets/MoonShot/Scripts/Photos/Goals/RequirePOIVisible.cs
+++ Assets/MoonShot/Scripts/Photos/Goals/RequirePOIVisible.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Moonshot.Photos.Requirements
@@ -8,11 +10,20 @@ namespace Moonshot.Photos.Requirements
 	{
 		public PointOfInterest m_pointOfInterest;
 		public int m_minVisibility = 1;
+		public bool m_matchByName = false;
+		public string m_matchName = "";
 
 		public bool Check(PhotoEvaluator i_evaluator, PhotoEvaluator.Context i_context)
 		{
-			int visibility = i_evaluator.GetVisibility(m_pointOfInterest, i_context);
-			return visibility >= m_minVisibility;
+			if (m_matchByName)
+			{
+				return i_evaluator.GetVisiblePOIs(m_minVisibility, i_context).Any(poi => poi.gameObject.name.Equals(m_matchName, StringComparison.InvariantCultureIgnoreCase));
+			}
+			else
+			{
+				int visibility = i_evaluator.GetVisibility(m_pointOfInterest, i_context);
+				return visibility >= m_minVisibility;
+			}
 		}
 	}
 }
