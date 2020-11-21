893e11d2b06fb0927e32542a1ef74be36f690561
tjm <tjm@cantab.net>
2020-11-21 22:37:34 +0000

    Focal length, tilt and lens shift settings on camera.

---
+++ Assets/MoonShot/Scripts/Photos/PropagateCameraState.cs
@@ -0,0 +1,23 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropagateCameraState : MonoBehaviour
+{
+	[ReorderableList,Required]
+	public List<Camera> m_targets;
+
+	public float FocalLength { get; set; } = 20;
+	public float LensShiftX { get; set; } = 0;
+	public float LensShiftY { get; set; } = 0;
+
+    void LateUpdate()
+    {
+        foreach (var target in m_targets)
+		{
+			target.focalLength = FocalLength;
+			target.lensShift = new Vector2(LensShiftX, LensShiftY);
+		}
+    }
+}
---
+++ Assets/MoonShot/Scripts/Player/CameraRoller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Moonshot.Player
+{
+	public class CameraRoller : MonoBehaviour
+	{
+		public float m_rollSpeed = 90.0f;
+		public InputActionReference RollAction;
+		public InputActionReference UIRollAction;
+
+		void OnEnable()
+		{
+			transform.localRotation = Quaternion.identity;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float rollControl = RollAction.action.ReadValue<float>() + UIRollAction.action.ReadValue<float>();
+
+			float currentAngle = transform.localRotation.eulerAngles.z;
+			float newAngle = Mathf.Clamp(WrapAngleDegrees(180.0f + currentAngle - rollControl * m_rollSpeed * Time.unscaledDeltaTime) - 180.0f, -90.0f, 90.0f);
+
+			transform.localRotation = Quaternion.Euler(0.0f, 0.0f, newAngle);
+		}
+
+		private float WrapAngleDegrees(float angle)
+		{
+			float turns = angle / 360.0f;
+			float localTurns = turns - Mathf.Floor(turns);
+			return localTurns * 360.0f;
+		}
+
+	}
+}
