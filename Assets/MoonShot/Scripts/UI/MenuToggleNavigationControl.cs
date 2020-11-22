ff552fe698fb8f526711225d204f07712dd8d7b1
tjm <tjm@cantab.net>
2020-11-22 20:56:44 +0000

    UI: Prevent unwanted tab jumping when switching to the tabs from the main panel.

---
+++ Assets/MoonShot/Scripts/UI/MenuToggleNavigationControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Moonshot.UI
+{
+	[RequireComponent(typeof(Toggle))]
+	public class MenuToggleNavigationControl : MonoBehaviour
+	{
+		private void Start()
+		{
+			m_toggle = GetComponent<Toggle>();
+		}
+		private void Update()
+		{
+			var nav = m_toggle.navigation;
+
+			if (m_toggle.isOn)
+			{
+				// We're active, so let the user navigate to us.
+				nav.mode = Navigation.Mode.Automatic;
+			}
+			else
+			{
+				if (EventSystem.current.currentSelectedGameObject && m_toggle.group.ActiveToggles().Contains(EventSystem.current.currentSelectedGameObject.GetComponent<Toggle>()))
+				{
+					// We're off but one of the other toggles in our group is selected, so let the user navigate to us from it.
+					nav.mode = Navigation.Mode.Automatic;
+				}
+				else
+				{
+					// We're off and nothing in our toggle group is selected, so disable navigation to us.
+					nav.mode = Navigation.Mode.None;
+				}
+			}
+
+			m_toggle.navigation = nav;
+		}
+
+		private Toggle m_toggle;
+	}
+}
