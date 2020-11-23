6490f5d38bdf243e2cc06d976c7ed3434c72fae3
tjm <tjm@cantab.net>
2020-11-23 16:07:19 +0000

    Correctly sorting clouds on Endless Roil. Mostly sensible cloud particles (though not currently lit perfectly).

---
+++ Assets/MoonShot/Scripts/Planet/CloudSurfaceParamSetup.cs
@@ -0,0 +1,49 @@
+﻿using Moonshot.Player;
+using Moonshot.World;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.Planet
+{
+	[RequireComponent(typeof(Renderer))]
+	public class CloudSurfaceParamSetup : MonoBehaviour
+	{
+		public float m_radius = 40.0f;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			m_renderer = GetComponent<Renderer>();
+			m_clipSphereCenterID = Shader.PropertyToID("_ClipSphereCenter");
+			m_clipSphereRadiusID = Shader.PropertyToID("_ClipSphereRadius");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (m_block == null)
+			{
+				m_block = new MaterialPropertyBlock();
+			}
+
+			if (PlayerVehicle.Current)
+			{
+				LocalFrame lf = LocalFrame.Get(transform);
+				m_block.SetVector(m_clipSphereCenterID, transform.InverseTransformPoint(LocalFrame.TransformPointToLocal(lf, LocalFrame.GetGlobalPosition(PlayerVehicle.Current.transform))));
+				m_block.SetFloat(m_clipSphereRadiusID, m_radius);
+			}
+			else
+			{
+				m_block.SetFloat(m_clipSphereRadiusID, 0.0f);
+			}
+			m_renderer.SetPropertyBlock(m_block);
+		}
+
+		private Renderer m_renderer;
+		private MaterialPropertyBlock m_block = null;
+		private int m_clipSphereCenterID;
+		private int m_clipSphereRadiusID;
+
+	}
+}
