16ee98414618c4edc9261864619c6c9200087115
tjm <tjm@cantab.net>
2020-11-24 23:31:47 +0000

    LOD support for visual effects. (Currently only sun is hooked up to use it.)

---
+++ Assets/MoonShot/Scripts/UI/Settings/ParticleLODSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.UI
+{
+	public class ParticleLODSetting : MonoBehaviour
+	{
+		public int m_maxQuality = 4;
+
+		public float ParticleQuality
+		{
+			get { return (float)(m_maxQuality - ParticleLOD); }
+			set { ParticleLOD = Mathf.RoundToInt(m_maxQuality - value); }
+		}
+
+		public static int ParticleLOD = 0;
+	}
+}
---
+++ Assets/MoonShot/Scripts/World/VFXLOD.cs
@@ -0,0 +1,54 @@
+﻿using Moonshot.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Moonshot.World
+{
+	[RequireComponent(typeof(VisualEffect))]
+	public class VFXLOD : MonoBehaviour
+	{
+		public VisualEffectAsset[] m_lods;
+		public GameObject m_fallback;
+		// Start is called before the first frame update
+		void Start()
+		{
+			m_visualEffect = GetComponent<VisualEffect>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			int lod = GetRequestedLOD();
+			if (lod < m_lods.Length)
+			{
+				var asset = m_lods[lod];
+				if (asset != m_visualEffect.visualEffectAsset)
+				{
+					m_visualEffect.visualEffectAsset = asset;
+				}
+				m_visualEffect.enabled = true;
+				if (m_fallback)
+				{
+					m_fallback.SetActive(false);
+				}
+			}
+			else
+			{
+				m_visualEffect.enabled = false;
+				if (m_fallback)
+				{
+					m_fallback.SetActive(true);
+				}
+			}
+		}
+
+		private int GetRequestedLOD()
+		{
+			return Mathf.Max(0, ParticleLODSetting.ParticleLOD);
+		}
+
+		private VisualEffect m_visualEffect;
+	}
+}
