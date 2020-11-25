930af9e194e64238d4ae9b705e6b7e88a68610f9
tjm <tjm@cantab.net>
2020-11-25 02:03:42 +0000

    Skin tone slider.

---
+++ Assets/MoonShot/Scripts/Player/SkinToneSetup.cs
@@ -0,0 +1,43 @@
+﻿using Moonshot.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.Player
+{
+	[RequireComponent(typeof(Renderer))]
+	public class SkinToneSetup : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+			m_block = new MaterialPropertyBlock();
+			m_renderer = GetComponent<Renderer>();
+			m_baseColorID = Shader.PropertyToID("_BaseColor");
+			m_emissiveColorID = Shader.PropertyToID("_EmissionColor");
+			m_metallicID = Shader.PropertyToID("_Metallic");
+			m_smoothnessID = Shader.PropertyToID("_Smoothness");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			var tone = SkinToneSetting.Tone;
+			if (tone != null)
+			{
+				m_block.SetColor(m_baseColorID, tone.m_primaryColor);
+				m_block.SetColor(m_emissiveColorID, tone.m_emissiveColor);
+				m_block.SetFloat(m_metallicID, tone.m_metallic);
+				m_block.SetFloat(m_smoothnessID, tone.m_smoothness);
+				m_renderer.SetPropertyBlock(m_block);
+			}
+		}
+
+		private Renderer m_renderer;
+		private MaterialPropertyBlock m_block;
+		private int m_baseColorID;
+		private int m_emissiveColorID;
+		private int m_metallicID;
+		private int m_smoothnessID;
+	}
+}
---
+++ Assets/MoonShot/Scripts/UI/Settings/SkinToneSetting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Moonshot.UI
+{
+	public class SkinToneSetting : MonoBehaviour
+	{
+		[Serializable]
+		public class ToneSetup
+		{
+			[ColorUsage(false, false)]
+			public Color m_primaryColor = Color.black;
+			[ColorUsage(false, true)]
+			public Color m_emissiveColor = Color.black;
+			public float m_metallic = 0.0f;
+			public float m_smoothness = 0.5f;
+		}
+
+		public ToneSetup[] m_tones;
+
+		public float ToneIndex
+		{
+			set
+			{
+				int index = Mathf.Clamp(Mathf.RoundToInt(value), 0, m_tones.Length - 1);
+				Tone = m_tones[index];
+			}
+		}
+
+		public static ToneSetup Tone { get; private set; }
+
+		public void Start()
+		{
+			GetComponent<Slider>().maxValue = m_tones.Length - 1;
+		}
+	}
+}
