950a04b134c78d7fa2b682bca29134e8c849b94c
tjm <tjm@cantab.net>
2020-11-26 19:54:26 +0000

    Slight lateral head movement in ship.

--- Assets/MoonShot/Scripts/Ship/ShipHeadMovement.cs
+++ Assets/MoonShot/Scripts/Ship/ShipHeadMovement.cs
@@ -10,10 +10,12 @@ namespace Moonshot.Ship
 		public Vector3 m_maxAngles = new Vector3(20.0f, 20.0f, 30.0f);
 		public float m_bankPropWhenTurning = 0.2f;
 		public float m_turnBlendRate = 0.01f;
+		public Vector3 m_maxOffsets = new Vector3(0.1f, 0.1f, 0.1f);
 
 		private void Update()
 		{
 			var targetAngles = Vector3.zero;
+			var targetOffset = Vector3.zero;
 			var controls = transform.GetComponentInAncestors<ShipControls>();
 			if (controls)
 			{
@@ -22,6 +24,7 @@ namespace Moonshot.Ship
 				clampedLook.y = Mathf.Clamp(clampedLook.y, -1.0f, 1.0f);
 				clampedLook.z = Mathf.Clamp(clampedLook.z, -1.0f, 1.0f);
 				targetAngles = Vector3.Scale(clampedLook, new Vector3(-1.0f, 1.0f, -1.0f));
+				targetOffset = -1.0f * Vector3.Scale(controls.Move, m_maxOffsets);
 			}
 			targetAngles.z += m_bankPropWhenTurning * targetAngles.y;
 			targetAngles.Scale(m_maxAngles);
@@ -34,6 +37,8 @@ namespace Moonshot.Ship
 			m_currentAngles.z = Mathf.Clamp(m_currentAngles.z, -m_maxAngles.z, m_maxAngles.z);
 
 			transform.localEulerAngles = m_currentAngles;
+
+			transform.localPosition = Vector3.Lerp(transform.localPosition, targetOffset, 0.01f);
 		}
 
 		private Vector3 m_currentAngles = Vector3.zero;
