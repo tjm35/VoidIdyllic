28e3d962b9512a71e178566f0eed02cf8adeec2a
tjm <tjm@cantab.net>
2020-11-27 13:21:50 +0000

    Fade out game sound effects when the ending sequence plays (but leave music going, for now?).

---
+++ Assets/Totality/PlayMakerIntegration/SetMixerParam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using HutongGames.PlayMaker;
+
+namespace Totality.PlayMakerIntegration.Mixer
+{
+	[ActionCategory("InputSystem")]
+	public class SetMixerParam : FsmStateAction
+	{
+		[RequiredField]
+		public AudioMixer m_mixer;
+		[RequiredField]
+		public FsmString m_paramName;
+
+		public FsmFloat m_value;
+		public bool m_everyFrame;
+
+		public override void Reset()
+		{
+			m_mixer = null;
+			m_paramName = "";
+			m_value = 0.0f;
+			m_everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			if (!m_everyFrame)
+			{
+				m_mixer.SetFloat(m_paramName.Value, m_value.Value);
+				Finish();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			m_mixer.SetFloat(m_paramName.Value, m_value.Value);
+		}
+
+	}
+}
