27aa94d8ebe881dd11b89db1ffbedf8db2ba0613
tjm <tjm@cantab.net>
2020-11-28 16:23:26 +0000

    Fix lighting on grass.

---
+++ Assets/MoonShot/Scripts/Planet/GrassVFXParamSetup.cs
@@ -0,0 +1,28 @@
+﻿using Moonshot.Player;
+using Moonshot.World;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Moonshot.Planet
+{
+	[ExecuteAlways]
+	[RequireComponent(typeof(VisualEffect))]
+	public class GrassVFXParamSetup : MonoBehaviour
+	{
+		private void Start()
+		{
+			m_vfx = GetComponent<VisualEffect>();
+			m_lightPosLocalID = Shader.PropertyToID("LightPosLocal");
+		}
+
+		private void LateUpdate()
+		{
+			LocalFrame frame = LocalFrame.Get(transform);
+			m_vfx.SetVector3(m_lightPosLocalID, transform.InverseTransformPoint(LocalFrame.TransformPointToLocal(frame, Vector3.zero)));
+		}
+
+		private VisualEffect m_vfx;
+		private int m_lightPosLocalID;
+	}
+}
