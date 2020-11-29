f561f760796d40fb8ec7806fd5e7c2899fd6460f
tjm <tjm@cantab.net>
2020-11-29 14:56:19 +0000

    Partially-working transit hub.

---
+++ Assets/MoonShot/Scripts/Props/Artifacts/TransitControlConsole.cs
@@ -0,0 +1,18 @@
+﻿using Moonshot.Photos;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.Props
+{
+	public class TransitControlConsole : MonoBehaviour
+	{
+		public TransitCore m_core;
+		public int m_destID = 0;
+
+		public void OnPhotoTaken(PointOfInterest i_poi)
+		{
+			m_core.Activate(m_destID);
+		}
+	}
+}
---
+++ Assets/MoonShot/Scripts/Props/Artifacts/TransitCore.cs
@@ -0,0 +1,56 @@
+﻿using OVR;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonshot.Props
+{
+	public class TransitCore : MonoBehaviour
+	{
+		public SoundFXRef m_ruinFX;
+		public GameObject[] m_chunkObjects;
+		public float m_activeTime = 3.0f;
+		public float m_audioFadeTime = 0.1f;
+
+		public void Activate(int i_dest)
+		{
+			for (int i = 0; i < m_chunkObjects.Length; ++i)
+			{
+				m_chunkObjects[i].SetActive(i == i_dest);
+			}
+			if (m_fxID != -1)
+			{
+				AudioManager.FadeOutSound(m_fxID, m_audioFadeTime);
+			}
+			m_fxID = m_ruinFX.PlaySound();
+			m_activeTimer = 0.0f;
+			m_active = true;
+		}
+
+		private void Update()
+		{
+			if (m_active)
+			{
+				m_activeTimer += Time.deltaTime;
+				if (m_activeTimer > m_activeTime)
+				{
+					for (int i = 0; i < m_chunkObjects.Length; ++i)
+					{
+						m_chunkObjects[i].SetActive(false);
+					}
+					if (m_fxID != -1)
+					{
+						AudioManager.FadeOutSound(m_fxID, m_audioFadeTime);
+					}
+					m_active = false;
+
+					// TODO: Teleport player if they are in zone.
+				}
+			}
+		}
+
+		private bool m_active = false;
+		private int m_fxID = -1;
+		private float m_activeTimer = 0.0f;
+	}
+}
